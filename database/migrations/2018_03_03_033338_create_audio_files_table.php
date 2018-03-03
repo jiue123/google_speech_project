8d49804d53343994fdd97dc54fa2c511b2b5ba39
PhatLe <thanhphat.le@citynow.vn>
2018-03-03 11:21:35 +0700

    push code first time 2018/03/03

---
+++ database/migrations/2018_03_03_033338_create_audio_files_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAudioFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('audio_files', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('RESTRICT')->onDelete('RESTRICT');
+            $table->string('name');
+            $table->string('path');
+            $table->string('path_flac');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('audio_files');
+    }
+}
